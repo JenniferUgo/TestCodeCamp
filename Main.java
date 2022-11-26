123bed0b88b0ab6393958dadad650c39ab877abe
JenniferUgo <JENNIFERUGO.NN@GMAIL.COM>
2022-11-26 21:41:20 +0100

    Selenium lines of code T14

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("This is my first Java code!");
+        System.out.println("I'm so excited to start learning Java!");
+        System.out.println("I'm pretty excited!");
+        System.out.println("Looking forward to being a sought-after Software Tester");
+        System.out.println("Ready to launch into the global tech market!");
+        System.out.println("Quality over Quantity");
+        System.out.println("Leave no man behind");
+        System.out.println("Team Postman Rocks 😁😁!");
+        System.out.println("WE MOVE!");
+    }
+}
